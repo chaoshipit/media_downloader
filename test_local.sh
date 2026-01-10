0d75d54940d298e29753e65ad6d5415d58003b91
chaozi <chaozi0327@163.com>
2026-01-10 18:00:15 +0800

    完成接口

---
+++ test_local.sh
@@ -0,0 +1,9 @@
+curl -X POST "http://localhost:9000/api/r2/parse_and_upload" \
+  -H "Content-Type: application/json" \
+  -H "Auth: shipit2026" \
+  -d '{
+    "url": "https://www.douyin.com/jingxuan?modal_id=7589851127722593546",
+    "quality": "high"
+  }'
+
+
